8edc37386e2ea0eaa35d05f56e474d0dd13ebc60
Volodya <skochilovvladimir@yandex.ru>
2023-03-17 01:10:13 +0700

    n43

---
+++ DZn43/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите b1, k1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2, k2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+double k2 = Convert.ToDouble(Console.ReadLine());
+double x = -(b1-b2)/(k1-k2);
+double y = k1*x + b1;
+System.Console.WriteLine($"пересечение в точке: ({x};{y})");
