e3db2968cc375e6878e854544290016cd6a9cce6
Volodya <skochilovvladimir@yandex.ru>
2023-03-25 00:26:39 +0700

    n56

---
+++ DZn56/Program.cs
@@ -0,0 +1,60 @@
+﻿Console.WriteLine("n");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("m");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+RandomNumberArray(matrix);
+PrintArray(matrix);
+SumStringMatrix(matrix);
+
+void RandomNumberArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void SumStringMatrix(int[,] array)
+{
+    int index = 0, minsum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        Console.WriteLine($"Сумма {i + 1} строки = {sum}");
+        if (i == 0)
+        {
+            minsum = sum;
+        }
+        else if (sum < minsum)
+        {
+            minsum = sum;
+            index = i;
+        }
+    }
+    string line = string.Empty;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        line += array[index, j] + " ";
+    }
+    Console.WriteLine($"Строка с минимальной суммой элементов равна {line}. ");
+}
